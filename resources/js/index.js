9a9cc535723a2b9feb17d91d81913c8320a16c67
Jose De La Cruz <jose.delcruz0490@icloud.com>
2021-03-02 23:46:43 -0400

    Added all the cards from API

--- resources/js/index.js
+++ resources/js/index.js
@@ -5,19 +5,32 @@ $.ajax({
   url: 'https://www.themealdb.com/api/json/v1/1/search.php?s',
   dataType: 'json',
 }).done(function (data) {
-  // console.log(data.meals[0]);
-  $.map(data, function (meal, i) {
+  data.meals.forEach((meal) => {
     $('.recipe-cards').append(
       `<div class='recipe-wrapper card'>
           <div class='recipe'>
-            <img class='recipe__img' src="${meal[0].strMealThumb}"/>
-            <h3>Meal: ${meal[0].strMeal} </h3>
+            <img class='recipe__img' src="${meal.strMealThumb}"/>
+            <h3>Meal: ${meal.strMeal} </h3>
             <span class='recipe__info'>
-              <span>Origin: ${meal[0].strArea}</span>
-              <span>Category: ${meal[0].strCategory}</span>
+              <span>Origin: ${meal.strArea}</span>
+              <span>Category: ${meal.strCategory}</span>
             </span>
           </div>
       </div>`
     );
   });
+  // $.map(data, function (meal, i) {
+  //   $('.recipe-cards').append(
+  //     `<div class='recipe-wrapper card'>
+  //         <div class='recipe'>
+  //           <img class='recipe__img' src="${meal[0].strMealThumb}"/>
+  //           <h3>Meal: ${meal[0].strMeal} </h3>
+  //           <span class='recipe__info'>
+  //             <span>Origin: ${meal[0].strArea}</span>
+  //             <span>Category: ${meal[0].strCategory}</span>
+  //           </span>
+  //         </div>
+  //     </div>`
+  //   );
+  // });
 });
